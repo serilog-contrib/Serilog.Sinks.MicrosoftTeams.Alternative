c0444d1e451872e2cd0e410117475eacffdd07eb
SeppPenner <HansM2013@gmx.de>
2021-10-04 20:00:52 +0200

    Updated push package script.

--- PushPackage.bat
+++ PushPackage.bat
@@ -1,2 +1,3 @@
 dotnet nuget push "src\Serilog.Sinks.MicrosoftTeams.Alternative\bin\Release\Serilog.Sinks.MicrosoftTeams.Alternative.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
+dotnet nuget push "src\Serilog.Sinks.MicrosoftTeams.Utf8Json\bin\Release\Serilog.Sinks.MicrosoftTeams.Utf8Json.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
 PAUSE
