69a9d574b42b2fb4cb7283829425f51e76d1f68c
SeppPenner <HansM2013@gmx.de>
2022-06-01 17:04:38 +0200

    Added some missing comments.

--- src/Serilog.Sinks.MicrosoftTeams.Alternative.Tests/Extensions/WireMockExtensions.cs
+++ src/Serilog.Sinks.MicrosoftTeams.Alternative.Tests/Extensions/WireMockExtensions.cs
@@ -1,7 +1,23 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WireMockExtensions.cs" company="SeppPenner and the Serilog contributors">
+// The project is licensed under the MIT license.
+// </copyright>
+// <summary>
+//   An extension class for WireMock tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace Serilog.Sinks.MicrosoftTeams.Alternative.Tests.Extensions;
 
+/// <summary>
+/// An extension class for WireMock tests.
+/// </summary>
 internal static class WireMockExtensions
 {
+    /// <summary>
+    /// Adds a default channel to the WireMock server.
+    /// </summary>
+    /// <param name="server">The WireMock server.</param>
     public static void AddDefaultChannel(this WireMockServer server)
     {
         server.Given(
@@ -18,6 +34,11 @@ internal static class WireMockExtensions
             );
     }
 
+    /// <summary>
+    /// Adds a channel to the WireMock server.
+    /// </summary>
+    /// <param name="server">The WireMock server.</param>
+    /// <param name="channel">The channel.</param>
     public static void AddChannel(this WireMockServer server, string channel)
     {
         server.Given(
