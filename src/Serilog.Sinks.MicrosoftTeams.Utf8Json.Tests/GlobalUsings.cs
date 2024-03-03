4539e06e0ab2d5f20a514dd783188c3a887290d6
SeppPenner <HansM2013@gmx.de>
2024-03-03 20:30:02 +0100

    Removed UTF8 sink.

--- src/Serilog.Sinks.MicrosoftTeams.Utf8Json.Tests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
-global using System.Diagnostics;
-
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-global using Serilog.Debugging;
-global using Serilog.Events;
-global using Serilog.Sinks.MicrosoftTeams.Alternative;
-#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
